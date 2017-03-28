709053cb90b39ca1c60d3c188f136250c8c730f5
Diego Dalpra <diego_felipe_miranda@hotmail.com>
2017-03-27 21:48:11 -0300

    mechido com crud

--- src/BigodonBundle/Form/ServicoType.php
+++ src/BigodonBundle/Form/ServicoType.php
@@ -5,6 +5,10 @@ namespace BigodonBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ServicoType extends AbstractType 
 {
@@ -13,10 +17,22 @@ class ServicoType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nome')
-                ->add('descricao')
-                ->add('valor')
-                ->add('duracao');
+        $builder->add('nome', TextType::class, array(
+                    "label" => "Nome:", "label_attr" => array("class" => "msg-erro")
+                ))
+                ->add('descricao', TextareaType::class, array (
+                    "label" => "Descrição do Serviço:"
+                ))
+                
+                ->add('valor', MoneyType::class, array(
+                "currency" => "BRL",
+                "grouping" =>  1, 
+                "label"    => "Valor (Reais):"
+                ))
+                
+                ->add('duracao', IntegerType::class, array (
+                    "label" => "Duração do Serviço (Minutos):"
+                ));
     }
     
     /**
